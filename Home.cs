ea70546507f1adef98ba1608e8be9bb536f965e6
96lenny96 <85729572+96lenny96@users.noreply.github.com>
2021-07-22 20:04:58 +0200

    Add files via upload

---
+++ Home.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (CrossPlatformInputManager.GetButtonDown ("play"))
+        {
+            SceneManager.LoadScene ("Main Menu");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown ("play"))
+        {
+            SceneManager.LoadScene ("Lvl1");
+        }
+    }
+}
